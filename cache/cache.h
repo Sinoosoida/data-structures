a38c0a092a6a41fd5e04bace2f65335d7ff9231d
Sinoosoida <bokhonko.ma@phystech.edu>
2023-08-02 23:19:44 +0300

    adding comments

--- cache/cache.h
+++ cache/cache.h
@@ -3,45 +3,78 @@
 #include <list>
 #include <functional>
 
+// LRUCache class template implementing a least recently used cache
 template<typename KeyType, typename ValueType>
 class LRUCache {
-    public:
-    LRUCache(int capacity, std::function<ValueType(const KeyType&)> fetchFunction)
-        : _capacity(capacity), _fetchFunction(fetchFunction) {}
+public:
 
-    void put(const KeyType& key, const ValueType& value) {
-        auto it = _cacheItemsMap.find(key);
-        if (it != _cacheItemsMap.end()) {
-            _cacheItemsList.erase(it->second);
-            _cacheItemsMap.erase(it);
-        }
+  // Constructor takes cache capacity and fetch function
+  LRUCache(int capacity, std::function<ValueType(const KeyType&)> fetchFunction)
+      : _capacity(capacity), _fetchFunction(fetchFunction) {}
 
-        _cacheItemsList.push_front({ key, value });
-        _cacheItemsMap[key] = _cacheItemsList.begin();
+  // Puts key-value pair in cache, moves pair to front of list
+  void put(const KeyType& key, const ValueType& value) {
 
-        if (_cacheItemsMap.size() > _capacity) {
-            auto last = _cacheItemsList.end();
-            last--;
-            _cacheItemsMap.erase(last->first);
-            _cacheItemsList.pop_back();
-        }
+    // Check if key already exists
+    auto it = _cacheItemsMap.find(key);
+    if (it != _cacheItemsMap.end()) {
+
+      // If so, remove old pair
+      _cacheItemsList.erase(it->second);
+      _cacheItemsMap.erase(it);
     }
 
-    const ValueType* get(const KeyType& key) {
-        auto it = _cacheItemsMap.find(key);
-        if (it == _cacheItemsMap.end()) {
-            ValueType value = _fetchFunction(key);
-            put(key, value);
-            it = _cacheItemsMap.find(key);
-        }
+    // Insert new pair at front
+    _cacheItemsList.push_front({key, value});
+    _cacheItemsMap[key] = _cacheItemsList.begin();
 
-        _cacheItemsList.splice(_cacheItemsList.begin(), _cacheItemsList, it->second);
-        return &(it->second->second);
+    // If over capacity, remove least recently used item
+    if (_cacheItemsMap.size() > _capacity) {
+      auto last = _cacheItemsList.end();
+      last--;
+      _cacheItemsMap.erase(last->first);
+      _cacheItemsList.pop_back();
+    }
+  }
+
+  // Gets value for key, returns nullptr if not found
+  // Moves found pair to front of list
+  const ValueType* get(const KeyType& key) {
+
+    // Lookup list iterator in map
+    auto it = _cacheItemsMap.find(key);
+
+    // Not found
+    if (it == _cacheItemsMap.end()) {
+
+      // Fetch value using fetch function
+      ValueType value = _fetchFunction(key);
+
+      // Insert this key-value pair
+      put(key, value);
+
+      // Lookup again to get list iterator
+      it = _cacheItemsMap.find(key);
     }
 
-    private:
-    std::list<std::pair<KeyType, ValueType>> _cacheItemsList;
-    std::unordered_map<KeyType, typename std::list<std::pair<KeyType, ValueType>>::iterator> _cacheItemsMap;
-    int _capacity;
-    std::function<ValueType(const KeyType&)> _fetchFunction;
+    // Splice found node to front to mark most recent
+    _cacheItemsList.splice(_cacheItemsList.begin(), _cacheItemsList, it->second);
+
+    return &(it->second->second);
+  }
+
+private:
+
+  // List storing key-value pairs ordered by recency
+  std::list<std::pair<KeyType, ValueType>> _cacheItemsList;
+
+  // Map from key to iterator in list for fast lookup
+  std::unordered_map<KeyType, typename std::list<std::pair<KeyType,ValueType>>::iterator> _cacheItemsMap;
+
+  // Max capacity of cache
+  int _capacity;
+
+  // Function to fetch value if not in cache
+  std::function<ValueType(const KeyType&)> _fetchFunction;
+
 };
