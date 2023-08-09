cf266240c3d320ba07f05c5d346ebdd7a869bb17
Sinoosoida <bokhonko.ma@phystech.edu>
2023-08-10 00:35:55 +0300

    The beauty of the code

--- memory-manager/mm.h
+++ memory-manager/mm.h
@@ -6,15 +6,18 @@
 #include <iostream>
 #include <new>
 
-#define free_color "\33[38;5;0;48;5;255m"
-#define allocated_color "\33[38;5;255;48;5;0m"
-#define reset_color "\33[m"
+// Цвета для вывода в консоль
+#define FREE_COLOR "\33[38;5;0;48;5;255m"
+#define ALLOCATED_COLOR "\33[38;5;255;48;5;0m"
+#define RESET_COLOR "\33[m"
 
+// Вызов конструктора для элемента
 template <class TYPE> inline void ConstructElements(TYPE *pElements) {
   memset(reinterpret_cast<void *>(pElements), 0, sizeof(TYPE));
   ::new (reinterpret_cast<void *>(pElements)) TYPE;
 }
 
+// Вызов деструктора для элемента
 template <class TYPE> inline void DestructElements(TYPE *pElements) {
   pElements->~TYPE();
   memset(reinterpret_cast<void *>(pElements), 0, sizeof(TYPE));
@@ -51,11 +54,11 @@ private:
   };
 
 public:
-  //  class CException {
-  //    // Не используется
-  //  public:
-  //    CException() {}
-  //  };
+  class CException {
+    // Не используется
+  public:
+    CException() {}
+  };
 
 public:
   /**
@@ -69,12 +72,14 @@ public:
         m_pCurrentBlk(nullptr),
         m_isDeleteElementsOnDestruct(isDeleteElementsOnDestruct) {}
 
+  // Вывод информации о блоках в консоль
   void print() {
-
     std::string separator = "|";
     std::string used = "X";
     std::string space = " ";
     size_t elements_per_line = 20;
+
+    std::cout << "\nMemory Manager Status\n";
 
     for (auto block = m_pBlocks; block != nullptr; block = block->pnext) {
       std::cout << "Block:\t" << block << "\tpoints to:\t" << block->pnext
@@ -106,7 +111,7 @@ public:
         std::cout << separator;
         for (int j = i; j < i + elements_per_line && j < m_blkSize; j++) {
           if (mask[j]) {
-            printf(free_color);
+            printf(FREE_COLOR);
             int out =
                 *(reinterpret_cast<int *>(block->pdata + j * step(sizeof(T))));
             for (int k = std::to_string(out).length();
@@ -114,14 +119,14 @@ public:
               std::cout << space;
             }
             std::cout << out;
-            printf(reset_color);
+            printf(RESET_COLOR);
             std::cout << separator;
           } else {
-            printf(allocated_color);
+            printf(ALLOCATED_COLOR);
             for (int k = 0; k < std::to_string(m_blkSize - 1).length(); k++) {
               std::cout << used;
             }
-            printf(reset_color);
+            printf(RESET_COLOR);
             std::cout << separator;
           }
         }
@@ -132,172 +137,169 @@ public:
 
   virtual ~CMemoryManager() { clear(); }
 
-  // Получить адрес нового элемента из менеджера
-  T *newObject() { // SGFM
+  T *newObject() {
 
-    if (m_pBlocks == nullptr) { // если блоков нет - создаём
+    // Если блоков еще нет, создадим первый
+    if (m_pBlocks == nullptr) {
       m_pBlocks = newBlock();
       m_pCurrentBlk = m_pBlocks;
-    } else { // блоки есть, нужно найти свободный или убедиться, что все заняты,
-             // и создать новый
-      if (m_pCurrentBlk->usedCount == m_blkSize) { // если текущий заполнен
-
-        block *last_block = nullptr;
-        block *tmp_block = nullptr;
-
-        if (m_pCurrentBlk->pnext == nullptr) {
-          last_block = m_pCurrentBlk;
-          tmp_block = m_pBlocks;
-        } else {
-          tmp_block = m_pCurrentBlk->pnext;
-        }
-
-        while (tmp_block->usedCount == m_blkSize &&
-               tmp_block != m_pCurrentBlk) { // перебираем все блоки
-          if (tmp_block->pnext ==
-              nullptr) { // достигли конца - начинаем с начала
-            last_block = tmp_block;
-            tmp_block = m_pBlocks;
-          } else {
-            tmp_block = tmp_block->pnext;
-          }
-        }
-        if (tmp_block == m_pCurrentBlk) { // дошли до блока, с которого
-                                          // начинали, значит все заполнены
-          last_block->pnext =
-              newBlock(); // создаём и добавляем в конец новый блок
-          m_pCurrentBlk = last_block->pnext; // теперь в нём будем место
-                                             // выделять
-        } else {
-          m_pCurrentBlk = tmp_block; // будем выделять место в незаполненом
-                                     // блоке
-        }
-      }
-      // m_pCurrentBlk - незаполненый блок, который мы и будем заполнять
-
-      //      if (m_pCurrentBlk->usedCount == m_blkSize) {//текущий заполнен,
-      //      найдём другой
-      //        auto tmp = m_pBlocks;
-      //        while ((tmp != nullptr) && (tmp->usedCount == m_blkSize)) {
-      //          tmp = tmp->pnext;
-      //        }
-      //        if (tmp == nullptr) { // если свободного блока не нашлось, то
-      //        создаём
-      //          while (m_pCurrentBlk->pnext != nullptr) { // находим последний
-      //          блок
-      //            m_pCurrentBlk = m_pCurrentBlk->pnext;
-      //          }
-      //          m_pCurrentBlk->pnext = newBlock();
-      //          m_pCurrentBlk = m_pCurrentBlk->pnext;
-      //        } else {
-      //          m_pCurrentBlk = tmp;
-      //        }
-      //      }
     }
 
-    // теперь у нас есть свободный блок
+    // Ищем незаполненный блок
+    else if (m_pCurrentBlk->usedCount == m_blkSize) {
 
+      // Перебираем все блоки в поисках незаполненного
+      block *last_block = nullptr;
+      block *tmp_block = m_pCurrentBlk->pnext;
+
+      if (!tmp_block) {
+        tmp_block = m_pBlocks;
+        last_block = m_pCurrentBlk;
+      }
+
+      while (tmp_block->usedCount == m_blkSize && tmp_block != m_pCurrentBlk) {
+
+        if (!tmp_block->pnext) {
+          last_block = tmp_block;
+          tmp_block = m_pBlocks;
+        } else {
+          tmp_block = tmp_block->pnext;
+        }
+      }
+
+      // Если нашли незаполненный, сохраняем его
+      if (tmp_block != m_pCurrentBlk) {
+        m_pCurrentBlk = tmp_block;
+
+        // Иначе создаем новый блок
+      } else {
+        last_block->pnext = newBlock();
+        m_pCurrentBlk = last_block->pnext;
+      }
+    }
+
+    // Берем первый свободный элемент из текущего блока
     T *FreeSpace =
         m_pCurrentBlk->pdata + m_pCurrentBlk->firstFreeIndex * step(sizeof(T));
-    int firstFreeIndex = *reinterpret_cast<int *>(
-        FreeSpace); // узнаём, что хранится в свободной ячейке - позиция
-                    // следующего свободного элемента
+    int firstFreeIndex = *(reinterpret_cast<int *>(FreeSpace));
+
+    // Обновляем индекс первого свободного
     m_pCurrentBlk->firstFreeIndex = firstFreeIndex;
     m_pCurrentBlk->usedCount++;
 
-    // вызываем конструктор элемента
+    // Вызываем конструктор элемента
     ConstructElements(FreeSpace);
-    return (FreeSpace);
+
+    return FreeSpace;
   }
 
-  // Освободить элемент в менеджере
-  bool deleteObject(T *p) {
-    // найдём блок, где лежит элемент
-    block *blk = m_pBlocks;
-    while (blk &&
-           !(p >= blk->pdata &&
-             p < blk->pdata +
-                     m_blkSize * step(sizeof(T)))) { // перебираем все блоки
-      blk = blk->pnext;
+  // Поиск блока по указателю на элемент
+  block *findBlock(T *elementPtr) {
+
+    block *currentBlock = m_pBlocks;
+
+    while (currentBlock &&
+           !(elementPtr >= currentBlock->pdata &&
+             elementPtr < currentBlock->pdata + m_blkSize * step(sizeof(T)))) {
+
+      currentBlock = currentBlock->pnext;
     }
 
-    if (blk == nullptr) { // нужного не нашли
+    return currentBlock;
+  }
+
+  bool deleteObject(T *elementPtr) {
+
+    // Поиск блока
+    block *currentBlock = findBlock(elementPtr);
+
+    // Если блок не найден - возвращаем ошибку
+    if (!currentBlock) {
       return false;
     }
 
-    // мы нашли кондидата
-    if ((reinterpret_cast<std::byte *>(p) -
-         reinterpret_cast<std::byte *>(blk->pdata)) %
+    // Проверка валидности указателя
+    if ((reinterpret_cast<std::byte *>(elementPtr) -
+         reinterpret_cast<std::byte *>(currentBlock->pdata)) %
             (sizeof(T) * step(sizeof(T))) !=
         0) {
-      return false; // указатель как бы в блоке, но при этом не в том месте, где
-                    // он мг бы быть выделен
+
+      return false;
     }
 
-    int FreeIndex = blk->firstFreeIndex; // перебираем пустые места
-    while (FreeIndex != -1) {
-      if ((blk->pdata + FreeIndex * step(sizeof(T))) ==
-          p) { // если указатель указывает на пустое место
-        return false; // то элемент уже был удалён, повторно удалить не сможем
+    // Поиск в списке свободных элементов
+    int freeIndex = currentBlock->firstFreeIndex;
+
+    while (freeIndex != -1) {
+
+      if (currentBlock->pdata + freeIndex * step(sizeof(T)) == elementPtr) {
+        return false;
       }
-      FreeIndex =
-          *reinterpret_cast<int *>(blk->pdata + FreeIndex * step(sizeof(T)));
+
+      freeIndex = *(reinterpret_cast<int *>(currentBlock->pdata +
+                                            freeIndex * step(sizeof(T))));
     }
 
-    // Элемент в блоке, и он не удалён. Можно удалять.
-    DestructElements(p); // вероятно, можно удалить
+    // Вызов деструктора
+    DestructElements(elementPtr);
 
-    auto second_free_index = blk->firstFreeIndex;
-    blk->firstFreeIndex = (p - blk->pdata) / step(sizeof(T));
-    *(reinterpret_cast<int *>((blk->pdata) +
-                              (blk->firstFreeIndex) * step(sizeof(T)))) =
-        second_free_index;
-    blk->usedCount--;
+    // Добавление в список свободных
+    auto secondFreeIndex = currentBlock->firstFreeIndex;
+    currentBlock->firstFreeIndex =
+        (elementPtr - currentBlock->pdata) / step(sizeof(T));
+
+    *(reinterpret_cast<int *>(currentBlock->pdata +
+                              currentBlock->firstFreeIndex * step(sizeof(T)))) =
+        secondFreeIndex;
+
+    // Уменьшаем счетчик использованных ячеек
+    currentBlock->usedCount--;
+
     return true;
   }
 
   // Очистка данных, зависит от m_isDeleteElementsOnDestruct
   void clear() {
-    if (m_pBlocks ==
-        nullptr) { // если блоки не были выделены, то и отчищать нечего
+    if (m_pBlocks == nullptr) {
       return;
     }
 
     if (m_isDeleteElementsOnDestruct) { // если мы хотим удалять элементы
 
-      bool *empty_mask =
-          new bool[m_blkSize]; // будем запоменать все свободные ячейки
+      bool *empty_mask = new bool[m_blkSize];
 
-      while (m_pBlocks != nullptr) { // проходимся по всем блокам
+      while (m_pBlocks != nullptr) {
         block *tmp = m_pBlocks->pnext;
 
         for (int i = 0; i < m_blkSize; i++) {
           empty_mask[i] = true;
         }
 
+        // запоминаем свободные ячейки
         int FreeSpaceIndex = m_pBlocks->firstFreeIndex;
-        while (FreeSpaceIndex !=
-               -1) { // проходимся по всем алоцированым элементам в блоке
-          empty_mask[FreeSpaceIndex] = false; // запоминаем свободные
+        while (FreeSpaceIndex != -1) {
+          empty_mask[FreeSpaceIndex] = false;
           FreeSpaceIndex = *(reinterpret_cast<int *>(
               m_pBlocks->pdata + FreeSpaceIndex * step(sizeof(T))));
         }
 
+        // проходимся по занятым элементам
         for (int i = 0; i < m_blkSize; i++) {
-          if (empty_mask[i]) { // проходимся по занятым элементам
-            DestructElements(m_pBlocks->pdata +
-                             i * step(sizeof(T))); // вызываем деструктор
+          if (empty_mask[i]) {
+            auto ptr = m_pBlocks->pdata + i * step(sizeof(T));
+            DestructElements(ptr);
           }
         }
 
-        deleteBlock(m_pBlocks); // теперь удаляем блок
+        // теперь удаляем блок
+        deleteBlock(m_pBlocks);
         m_pBlocks = tmp;
       }
 
     } else {
-      while (m_pBlocks != nullptr) { // проходимся по всем блокам
+      while (m_pBlocks != nullptr) {
         block *tmp = m_pBlocks->pnext;
-        deleteBlock(m_pBlocks); // теперь удаляем блок
+        deleteBlock(m_pBlocks);
         m_pBlocks = tmp;
       }
     }
@@ -314,15 +316,17 @@ private:
     new_block->pnext = nullptr;
 
     // Выделение памяти без вызова конструктора элемента
-    new_block->pdata = reinterpret_cast<T *>(
-        ::operator new(m_blkSize * sizeof(T) * step(sizeof(T))));
+    auto len = m_blkSize * sizeof(T) * step(sizeof(T));
+    new_block->pdata = reinterpret_cast<T *>(::operator new(len));
 
+    // Заполнение индексами свободных элементов
     for (int i = 0; i < m_blkSize; i++) {
-      *(reinterpret_cast<int *>(new_block->pdata + i * step(sizeof(T)))) =
-          i + 1;
+      auto ptr = new_block->pdata + i * step(sizeof(T));
+      *(reinterpret_cast<int *>(ptr)) = i + 1;
     }
-    *(reinterpret_cast<int *>(new_block->pdata +
-                              (m_blkSize - 1) * step(sizeof(T)))) = -1;
+
+    auto ptr = new_block->pdata + (m_blkSize - 1) * step(sizeof(T));
+    *(reinterpret_cast<int *>(ptr)) = -1;
 
     return new_block;
   }
